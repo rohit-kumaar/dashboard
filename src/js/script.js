f3a0d22c7ea23f14b4289e5d00543dbfe28a3ae8
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-04-06 00:37:11 +0530

    Nav, Header, aside

--- src/js/script.js
+++ src/js/script.js
@@ -1,31 +1,12 @@
-document.addEventListener("readystatechange", (event) => {
-  if (event.target.readyState === "complete") {
-    initApp();
-  }
+
+
+// Header email section dropdown start
+function show(anything) {
+  document.querySelector(".textBox").value = anything;
+}
+
+const dropdown = document.querySelector(".dropdown");
+dropdown.addEventListener("click", () => {
+  dropdown.classList.toggle("active");
 });
-
-const initApp = () => {
-  let sidebar = document.querySelector(".sidebar");
-  let closeBtn = document.querySelector("#btn");
-  let searchBtn = document.querySelector(".bx-search");
-
-  closeBtn.addEventListener("click", () => {
-    sidebar.classList.toggle("open");
-    menuBtnChange(); //calling the function(optional)
-  });
-
-  searchBtn.addEventListener("click", () => {
-    // Sidebar open when you click on the search iocn
-    sidebar.classList.toggle("open");
-    menuBtnChange(); //calling the function(optional)
-  });
-
-  // following are the code to change sidebar button(optional)
-  function menuBtnChange() {
-    if (sidebar.classList.contains("open")) {
-      closeBtn.classList.replace("bx-menu", "bx-menu-alt-right"); //replacing the iocns class
-    } else {
-      closeBtn.classList.replace("bx-menu-alt-right", "bx-menu"); //replacing the iocns class
-    }
-  }
-};
+// Header email section dropdown end
